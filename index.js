f52e00ee48a6e1823e0ad01dba4817dc48f110a0
HoanTP <51874577+bedida9x@users.noreply.github.com>
2021-08-22 17:12:23 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-const express = require('express')
-const app = express()
-var port = process.env.PORT || 8080;
-const http = require('http')
-const server = http.createServer(app)
-app.use(express.static(__dirname + "/static"));
-const {
-    Server
-} = require('socket.io')
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-const io = new Server(server)
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
-io.on('connection', (socket) => {
-    console.log('User connected');
-    socket.on('on-chat', data => {
-        io.emit('user-chat', data)
-    })
-})
-
-server.listen(port, () => {
-    console.log('Listening on port 3000');
-})
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
